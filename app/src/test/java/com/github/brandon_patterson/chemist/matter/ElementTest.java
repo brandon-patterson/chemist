db013c59e647e6b5cd695c4098afc092949cdc90
bjpatterson <brandonpatterson99@yahoo.com>
2019-08-30 21:04:55 -0700

    test failure cases as well

--- app/src/test/java/com/github/brandon_patterson/chemist/matter/ElementTest.java
+++ app/src/test/java/com/github/brandon_patterson/chemist/matter/ElementTest.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 
 import com.github.brandon_patterson.chemist.R;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,9 +37,28 @@ public class ElementTest {
     }
 
     @Test
-    public void fromAtomicNumber() throws NoSuchElementException
+    public void fromAtomicNumber_validInput() throws NoSuchElementException
     {
         assertThat(Element.fromAtomicNumber(6)).isEqualTo(Element.C);
+    }
+
+    @Test
+    public void fromAtomicNumber_invalidInput()
+    {
+        String expectedExceptionName = NoSuchElementException.class.getSimpleName();
+        try {
+            Element.fromAtomicNumber(999);
+            Assert.fail("Expected " + expectedExceptionName + ", but no exception was thrown.");
+        }
+        catch (NoSuchElementException ex)
+        {
+            // pass
+        }
+        catch (Exception ex)
+        {
+            String exType = ex.getClass().getSimpleName();
+            Assert.fail("Expected " + expectedExceptionName + ", but got " + exType);
+        }
     }
 
     @Test
@@ -49,12 +69,31 @@ public class ElementTest {
     }
 
     @Test
-    public void fromSymbol() throws NoSuchElementException
+    public void fromSymbol_validInput() throws NoSuchElementException
     {
         when(mockResources.getString(R.string.element_symbol_079)).thenReturn("Au");
         assertThat(Element.fromSymbol("Au", mockContext)).isEqualTo(Element.AU);
         assertThat(Element.fromSymbol("au", mockContext)).isEqualTo(Element.AU);
         assertThat(Element.fromSymbol("AU", mockContext)).isEqualTo(Element.AU);
+    }
+
+    @Test
+    public void fromSymbol_invalidInput() throws NoSuchElementException
+    {
+        String expectedExceptionName = NoSuchElementException.class.getSimpleName();
+        try {
+            Element.fromSymbol("Xx", mockContext);
+            Assert.fail("Expected " + expectedExceptionName + ", but no exception was thrown.");
+        }
+        catch (NoSuchElementException ex)
+        {
+            // pass
+        }
+        catch (Exception ex)
+        {
+            String exType = ex.getClass().getSimpleName();
+            Assert.fail("Expected " + expectedExceptionName + ", but got " + exType);
+        }
     }
 
     @Test
@@ -65,11 +104,30 @@ public class ElementTest {
     }
 
     @Test
-    public void fromName() throws NoSuchElementException
+    public void fromName_validInput() throws NoSuchElementException
     {
         when(mockResources.getString(R.string.element_name_092)).thenReturn("Uranium");
         assertThat(Element.fromName("Uranium", mockContext)).isEqualTo(Element.U);
         assertThat(Element.fromName("uranium", mockContext)).isEqualTo(Element.U);
         assertThat(Element.fromName("URANIUM", mockContext)).isEqualTo(Element.U);
+    }
+
+    @Test
+    public void fromName_invalidInput()
+    {
+        String expectedExceptionName = NoSuchElementException.class.getSimpleName();
+        try {
+            Element.fromName("Adamantium", mockContext);
+            Assert.fail("Expected " + expectedExceptionName + ", but no exception was thrown.");
+        }
+        catch (NoSuchElementException ex)
+        {
+            // pass
+        }
+        catch (Exception ex)
+        {
+            String exType = ex.getClass().getSimpleName();
+            Assert.fail("Expected " + expectedExceptionName + ", but got " + exType);
+        }
     }
 }
