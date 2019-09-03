2012ee14d6b6d670e970f46ba59f6e5333465040
zspatter <localhost@zspatter.dev>
2019-09-03 09:41:45 -0400

    Merge branch 'develop' into home-page

--- app/src/androidTest/java/com/github/brandon_patterson/chemist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/brandon_patterson/chemist/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.gitub.brandon_patterson.chemist;
+package com.github.brandon_patterson.chemist;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.gitub.brandon_patterson.chemist", appContext.getPackageName());
+        assertEquals("com.github.brandon_patterson.chemist", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/github/brandon_patterson/chemist/MainActivity.java
+++ app/src/main/java/com/github/brandon_patterson/chemist/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gitub.brandon_patterson.chemist;
+package com.github.brandon_patterson.chemist;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
