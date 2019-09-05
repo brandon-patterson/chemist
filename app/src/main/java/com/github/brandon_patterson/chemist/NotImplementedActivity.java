3cd3590d4db4ca17291b24d1524e1fe332143e74
Zachary Patterson <localhost@zspatter.dev>
2019-09-05 10:06:52 -0400

    Create home page (#12)
    * Adds buttons to home page
    
    * Adjusts layout of buttons on main page
    
    Follwoing feedback from @brandon-patterson
    
    * Removes `weightSum` and `layout_weight` attributes
    
    No longer neccesary with a vertical layout
    
    * Adds `NotImplemented` activity
    
    * Adds onClickListeners for buttons in MainActivity
    
    This allows the buttons to link to new pages
    
    * Merge branch 'develop' into home-page
    
    * Renames `NotImplemented` to `NotImplementedActivity`
    
    Additionally, adjusts imports to reflect updated namespace
    
    * Explicitly sizes buttons
    
    This allows for consistent button widths
    
    * Converts string literals to references to resource file
    
    * Adds homepage to changelog
    
    * Fixes accidental reversion from merge

---
+++ app/src/main/java/com/github/brandon_patterson/chemist/NotImplementedActivity.java
@@ -0,0 +1,20 @@
+package com.github.brandon_patterson.chemist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.github.brandon_patterson.chemist.MainActivity;
+import com.github.brandon_patterson.chemist.R;
+
+public class NotImplementedActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_implemented);
+    }
+}
