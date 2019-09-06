ec3b007660e7541048cdcd86bfb36fb61bd6fb82
brandon-patterson <brandonpatterson99@yahoo.com>
2019-09-06 09:32:02 -0700

    extract some redundant code (#29)

--- app/src/main/java/com/github/brandon_patterson/chemist/MainActivity.java
+++ app/src/main/java/com/github/brandon_patterson/chemist/MainActivity.java
@@ -1,12 +1,10 @@
 package com.github.brandon_patterson.chemist;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import com.github.brandon_patterson.chemist.NotImplementedActivity;
-import com.github.brandon_patterson.chemist.R;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,55 +16,27 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
 
-        Button unitAnalysisButton = (Button) findViewById(R.id.main_unit_analysis_button);
-        Button modelsButton = (Button) findViewById(R.id.main_models_button);
-        Button balanceEquationsButton = (Button) findViewById(R.id.main_balance_equations_button);
-        Button stoichiometryButton = (Button) findViewById(R.id.main_stoichiometry_button);
-        Button referencesButton = (Button) findViewById(R.id.main_references_button);
+        Button unitAnalysisButton = findViewById(R.id.main_unit_analysis_button);
+        Button modelsButton = findViewById(R.id.main_models_button);
+        Button balanceEquationsButton = findViewById(R.id.main_balance_equations_button);
+        Button stoichiometryButton = findViewById(R.id.main_stoichiometry_button);
+        Button referencesButton = findViewById(R.id.main_references_button);
 
-        unitAnalysisButton.setOnClickListener(new View.OnClickListener()
+        unitAnalysisButton.setOnClickListener(onClickGoTo(NotImplementedActivity.class));
+        modelsButton.setOnClickListener(onClickGoTo(NotImplementedActivity.class));
+        balanceEquationsButton.setOnClickListener(onClickGoTo(NotImplementedActivity.class));
+        stoichiometryButton.setOnClickListener(onClickGoTo(NotImplementedActivity.class));
+        referencesButton.setOnClickListener(onClickGoTo(NotImplementedActivity.class));
+    }
+
+    private View.OnClickListener onClickGoTo(final Class<?> activity)
+    {
+        return new View.OnClickListener()
         {
             @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, NotImplementedActivity.class));
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, activity));
             }
-        });
-
-        modelsButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, NotImplementedActivity.class));
-            }
-        });
-
-        balanceEquationsButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, NotImplementedActivity.class));
-            }
-        });
-
-        stoichiometryButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, NotImplementedActivity.class));
-            }
-        });
-
-        referencesButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                startActivity(new Intent(MainActivity.this, NotImplementedActivity.class));
-            }
-        });
+        };
     }
 }
